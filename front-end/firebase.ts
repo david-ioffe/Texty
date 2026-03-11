02e8c7d3401a2b8b884dd29829622960b0eeecf1
David Ioffe <david.ioffe@mail.huji.ac.il>
2026-03-11 13:50:15 +0200

    Changed front-end

--- front-end/firebase.ts
+++ front-end/firebase.ts
@@ -1,12 +1,15 @@
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "xxxxx",
-  authDomain: "xxxxx.firebaseapp.com",
-  projectId: "xxxxx",
-  storageBucket: "xxxxx.appspot.com",
-  messagingSenderId: "xxxxx",
-  appId: "xxxxx"
+  apiKey: "AIzaSyDOBkq_VrIvOs_t2-2VhwaVKvXylIUO5z8",
+  authDomain: "texty-ee5ff.firebaseapp.com",
+  projectId: "texty-ee5ff",
+  storageBucket: "texty-ee5ff.firebasestorage.app",
+  messagingSenderId: "73073852972",
+  appId: "1:73073852972:web:668f6ab637cd2f9879c789",
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
